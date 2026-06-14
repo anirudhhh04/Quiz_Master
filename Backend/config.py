4d6148d53b02eed39dc4c04e1363acda40b0a786
ANIRUDH P <anirudh.p@iouring.com>
2026-06-14 20:41:13 +0530

    Updations...

--- Backend/config.py
+++ Backend/config.py
@@ -1,8 +1,9 @@
 import os
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY","quizmaster-secret-key")
+    SECRET_KEY = os.getenv("SECRET_KEY", "quizmaster-secret-key")
     DB_HOST = os.getenv("DB_HOST")
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_NAME = os.getenv("DB_NAME")
+    DB_PORT = os.getenv("DB_PORT")
     JWT_SECRET_KEY = os.getenv( "JWT_SECRET_KEY", "jwt-secret-key")
--- Backend/db.py
+++ Backend/db.py
@@ -1,12 +1,11 @@
 import mysql.connector
 from config import Config
 def get_connection():
-
     connection = mysql.connector.connect(
         host=Config.DB_HOST,
         user=Config.DB_USER,
         password=Config.DB_PASSWORD,
-        database=Config.DB_NAME
+        database=Config.DB_NAME,
+        port=int(Config.DB_PORT)
     )
-
     return connection
