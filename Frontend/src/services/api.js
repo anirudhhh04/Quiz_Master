7d89ba0ed96c1e8bdca41e0095c7766fffcf65a8
ANIRUDH P <anirudh.p@iouring.com>
2026-06-15 22:37:20 +0530

    Updated URL

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-const api = axios.create({ baseURL: "http://127.0.0.1:5000",});
+const api = axios.create({ baseURL: "https://quiz-master-vqap.onrender.com",});
 
 export default api;   //for building api
